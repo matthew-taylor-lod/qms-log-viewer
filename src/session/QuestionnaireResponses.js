e9c80d39b70536a361f5d67644b9e7259798769e
Matt Taylor <matthew.taylor@lloydsonlinedoctor.com>
2021-02-17 01:40:31 +0000

    Removed tool tips and added static ids for questions and numbers

--- src/session/QuestionnaireResponses.js
+++ src/session/QuestionnaireResponses.js
@@ -1,13 +1,13 @@
 import "./QuestionnaireResponses.scss";
 import React from "react";
-import ReactTooltip from "react-tooltip";
 
 function QuestionnaireResponses({data}) {
     if (!data) return "None";
     console.log(data);
 
     const responses = [];
-    let i =0;
+    let i = 0;
+    let questionNumber = 1;
     while (i < data.length) {
         const response = data[i]
 
@@ -31,6 +31,7 @@ function QuestionnaireResponses({data}) {
                 j++;
             }
             responses.push(<MultipleChoiceResponse key={response.displayOrder}
+                                                   number={questionNumber}
                                                    question={response.question}
                                                    questionId={response.questionId}
                                                    answer={response.answer}
@@ -41,6 +42,7 @@ function QuestionnaireResponses({data}) {
         }
         else {
             responses.push(<Response key={response.displayOrder}
+                                     number={questionNumber}
                                      question={response.question}
                                      questionId={response.questionId}
                                      answer={response.answer}
@@ -49,53 +51,72 @@ function QuestionnaireResponses({data}) {
             }
 
         i++;
+        questionNumber++;
     }
 
     return (
         <div className="QuestionnaireResponses">
-            <ReactTooltip place="left" effect="solid" delayShow={250}/>
             <h2>Questionnaire Responses</h2>
-            <ol>
+            <div className="responses">
                 {responses}
-            </ol>
+            </div>
             <hr/>
             <div className="center">-- END OF QUESTIONNAIRE --</div>
         </div>
     )
 }
 
-function Response({question, questionId, answer, answerId, significant}) {
+function Response({number, question, questionId, answer, answerId, significant}) {
     const significantClass = (significant) ? "significant" : "";
     return (
-        <li className="Response">
-            <div className="question" dangerouslySetInnerHTML={{__html: question}} data-tip={"questionId: " + questionId} data-class="questionTooltip"></div>
-            <div className={"answer " +significantClass} data-tip={"answerId: "+ answerId} data-class="answerTooltip">
-                {answer}
+        <div className="Response">
+            <div className="cell">
+                <div className="number">{number}.</div>
+                <div className="questionId">questionId: {questionId}</div>
             </div>
-        </li>
+            <div className="cell">
+                <div className="question" dangerouslySetInnerHTML={{__html: question}}></div>
+                <span className="answerId">answerId: {answerId}</span>
+                <span className={"answer " + significantClass}>
+                    {answer}
+                </span>
+            </div>
+        </div>
     )
 }
 
-function MultipleChoiceResponse({question, questionId, answer, answerId, significant, choices}) {
+function MultipleChoiceResponse({number, question, questionId, answer, answerId, significant, choices}) {
     const answers = choices.map(c => {
             const significantClass = (c.significant) ? "significant" : "";
             return <tr key={c.key} data-tip={"answerId: "+ c.answerId} data-class="answerTooltip">
+                <td><span className="answerId">answerId: {c.answerId}</span></td>
                 <td className="question" dangerouslySetInnerHTML={{__html: c.question}}></td>
-                <td className={"answer " +significantClass}>{c.answer}</td>
+                <td className={"answer " + significantClass}>{c.answer}</td>
             </tr>
         }
     );
     const significantClass = (significant) ? "significant" : "";
     return (
-        <li className="Response" >
-            <div className="question" dangerouslySetInnerHTML={{__html: question}} data-tip={"questionId: " + questionId} data-class="questionTooltip"></div>
-            <div className={"answer " + significantClass} data-tip={"answerId: "+ answerId} data-class="answerTooltip">{answer}</div>
-            <table>
-                <tbody>
-                    {answers}
-                </tbody>
-            </table>
-        </li>
+        <div className="Response">
+            <div className="cell">
+                <div className="number">{number}.</div>
+                <div className="questionId">questionId: {questionId}</div>
+            </div>
+            <div className="cell">
+                <div className="question" dangerouslySetInnerHTML={{__html: question}}></div>
+                {answerId && <>
+                    <span className="answerId">answerId: {answerId}</span>
+                    <span className={"answer " +significantClass}>
+                        {answer}
+                    </span>
+                    </> }
+                <table>
+                    <tbody>
+                        {answers}
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 
